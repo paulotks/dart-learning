59ed437c0a413422bfc65be03547aac082f1d79d
Paulo Eduardo <81379525+paulotks@users.noreply.github.com>
2022-07-12 23:36:22 -0300

    Add files via upload

---
+++ aula1/variaveis.dart
@@ -0,0 +1,12 @@
+main() {
+  var n1 = 1;
+  int n3 = 2;
+  double n2 = 4.56;
+  var t1 = 'texto';
+
+  print(n1 + n2);
+
+  print(n1.runtimeType);
+  print(t1.runtimeType); //runtimeType informa o time de variavel
+  print(n3);
+}
